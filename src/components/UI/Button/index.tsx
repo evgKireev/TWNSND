f731d3ed3210d497ed6fb6d9b2e7a910f1cdf254
OlgaGrishchenko <olg.grishchenko@gmail.com>
2023-02-11 08:45:41 +0300

    update button

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,6 +1,37 @@
-import styles from './Button.module.scss'
-const Button = () => {
-  return <button></button>
-}
+import React, { FC, ReactElement } from "react";
 
-export default Button
+import styles from './Button.module.scss'
+import classNames from "classnames";
+
+export enum ButtonTypes {
+   Primary = "primary",
+   Secondary = "secondary",
+   Icons = "icons"
+};
+
+type ButtonProps = {
+   title: string | ReactElement;
+   type: ButtonTypes;
+   onClick?: () => void;
+   className?: string;
+   disabled?: boolean;
+};
+
+const Button: FC<ButtonProps> = (props) => {
+   const { type, title, onClick, className, disabled } = props;
+
+   const buttonClassName = styles[type];
+
+   return (
+      <div
+         className={classNames(styles.button, buttonClassName, className, {
+            [styles.disabled]: !!disabled,
+         })}
+         onClick={onClick}
+      >
+      {title}
+      </div>
+   );
+};
+
+export default Button;
