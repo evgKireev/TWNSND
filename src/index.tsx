7cf5182565ad5121aa840415ee7f9f9307827108
evgKireev <bob1_k@mail.ru>
2023-02-11 04:28:57 +0300

    add 80% input + validation

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './redux/store';
-import { HashRouter } from 'react-router-dom';
-import './index.scss';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import { store } from './redux/store'
+import { HashRouter } from 'react-router-dom'
+import './index.scss'
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById('root')
 
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
+  const root = ReactDOM.createRoot(rootEl)
   root.render(
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
-  );
+    </BrowserRouter>
+  )
 }
