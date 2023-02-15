52e748e2c2f91d7908d8098cc000cb6cc1623ea9
OlgaGrishchenko <olg.grishchenko@gmail.com>
2023-02-15 17:32:29 +0300

    fixed footer

--- src/components/APP/Footer/index.tsx
+++ src/components/APP/Footer/index.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { TelegramFooter } from '../../../assets/FooterIcons/TelegramFooter'
 import { VKFooter } from '../../../assets/FooterIcons/VKFooter'
 import { WhatsAppFooter } from '../../../assets/FooterIcons/WhatsAppFooter'
+import LogoWhite from '../../../assets/img/logoWhite'
 import styles from './Footer.module.scss'
 
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <div>LOGO</div>
-      <div className={styles.icons}>
-        <VKFooter />
-        <TelegramFooter />
-        <WhatsAppFooter />
+      <div><LogoWhite /></div>
+      <div className={styles.containerIcons}>
+        <div className={styles.icon}><VKFooter /></div>
+        <div className={styles.icon}><TelegramFooter /></div>
+        <div className={styles.icon}><WhatsAppFooter /></div>
       </div>
     </div>
   )
