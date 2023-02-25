07362ef364112e0543c80418b7d0e684f1066796
evgKireev <bob1_k@mail.ru>
2023-02-25 15:49:07 +0300

    deploy

--- src/@types/constant.ts
+++ src/@types/constant.ts
@@ -2,10 +2,10 @@ import { create } from 'apisauce'
 export const ACCESS_TOKEN_KEY = 'accessToken1'
 export const REFRESH_TOKEN_KEY = 'refreshToken1'
 export const API = create({
-  baseURL: 'https://744e-37-45-91-246.eu.ngrok.io/',
+  baseURL: 'https://f5ca-37-45-91-246.eu.ngrok.io/',
 })
 export const API_GOOGLE = create({
-  baseURL: 'https://744e-37-45-91-246.eu.ngrok.io/',
+  baseURL: 'https://f5ca-37-45-91-246.eu.ngrok.io/',
 })
 export const MY_URL = 'https://twnsnd.vercel.app'
 
