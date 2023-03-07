6b8561c775f6e647f98c15b506c96f24f066b031
evgKireev <bob1_k@mail.ru>
2023-03-07 21:38:53 +0300

    fix token

--- src/redux/Sagas/callCheckingUser.ts
+++ src/redux/Sagas/callCheckingUser.ts
@@ -15,41 +15,26 @@ export default function* callCheckingUser(api: any, ...rest: any) {
     sessionStorage.getItem(REFRESH_TOKEN_KEY) ||
     ''
   const res: ApiResponse<any> = yield call(api, accessToken, ...rest)
-  if (res.status === 401) {
-    const { ok, data, status }: ApiResponse<any> = yield call(
-      API.verifyToken,
-      accessToken
+  if (!res.ok && !res.data) {
+    const { data, ok }: ApiResponse<any> = yield call(
+      API.getNewAccessToken,
+      refreshToken
     )
     if (ok && data) {
-      if (data.isActive === false) {
-        const { data, ok }: ApiResponse<any> = yield call(
-          API.getNewAccessToken,
-          refreshToken
-        )
-        if (ok && data) {
-          const { access_token, refresh_token } = data
-          if (localStorage.getItem(ACCESS_TOKEN_KEY)) {
-            localStorage.setItem(ACCESS_TOKEN_KEY, access_token)
-            localStorage.setItem(ACCESS_TOKEN_KEY, refresh_token)
-          } else if (sessionStorage.getItem(ACCESS_TOKEN_KEY)) {
-            sessionStorage.setItem(ACCESS_TOKEN_KEY, access_token)
-            sessionStorage.setItem(ACCESS_TOKEN_KEY, refresh_token)
-          }
-          const newResponse: ApiResponse<any> = yield call(
-            api,
-            access_token,
-            ...rest
-          )
-          return newResponse
-        } else {
-          yield put(logoutUser())
-          toast.error(
-            'Срок действия вашего сеанса истек, пожалуйста, авторизуйтесь'
-          )
-        }
-      } else {
-        return res
+      const { access_token, refresh_token } = data
+      if (localStorage.getItem(ACCESS_TOKEN_KEY)) {
+        localStorage.setItem(ACCESS_TOKEN_KEY, access_token)
+        localStorage.setItem(ACCESS_TOKEN_KEY, refresh_token)
+      } else if (sessionStorage.getItem(ACCESS_TOKEN_KEY)) {
+        sessionStorage.setItem(ACCESS_TOKEN_KEY, access_token)
+        sessionStorage.setItem(ACCESS_TOKEN_KEY, refresh_token)
       }
+      const newResponse: ApiResponse<any> = yield call(
+        api,
+        access_token,
+        ...rest
+      )
+      return newResponse
     } else {
       yield put(logoutUser())
       toast.error(
