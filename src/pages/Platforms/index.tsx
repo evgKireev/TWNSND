a6b5c93b82051d579d53c610cfb26a50bdf57777
evgKireev <bob1_k@mail.ru>
2023-03-08 23:22:33 +0300

    check code

--- src/pages/Platforms/index.tsx
+++ src/pages/Platforms/index.tsx
@@ -4,19 +4,26 @@ import Search from '../../components/UI/Search'
 import styles from './Platforms.module.scss'
 import platforms from '../../platforms.json'
 import PlatformCard from '../../components/APP/PlatformCard'
+import ButtonNew, { ButtonNewTypes } from '../../components/UI/ButtonNew'
 
 const Platforms = () => {
   return (
-    <div className={styles.wrap}>
+    <div className={styles.platformWrap}>
       <Baner />
       <Search />
-
-      <div className={styles.platformsCardsWrapper}>
-        {platforms.map((platform) => {
-          return <PlatformCard platform={platform} key={platform.id} />
-        })}
+      <div className={styles.platformInner}>
+        <div className={styles.sort}>Sort</div>
+        <div className={styles.platformsCards}>
+          {platforms.map((platform) => {
+            return <PlatformCard platform={platform} key={platform.id} />
+          })}
+        </div>
       </div>
-
+      <ButtonNew
+        title={'Показать больше'}
+        type={ButtonNewTypes.Primary}
+        className={styles.btn}
+      />
       <PlatformInfo />
     </div>
   )
